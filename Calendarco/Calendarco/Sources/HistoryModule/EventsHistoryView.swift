7056737fdfe6a51a1282eedceaba5da1fac226ba
Domagoj Bunoza <domagoj.bunoza@rise-world.com>
2024-07-06 15:28:52 +0200

    Add delete all button

--- Calendarco/Calendarco/Sources/HistoryModule/EventsHistoryView.swift
+++ Calendarco/Calendarco/Sources/HistoryModule/EventsHistoryView.swift
@@ -7,6 +7,7 @@ struct EventsHistoryView: View {
     @Environment(\.openURL) var openURL
     @EnvironmentObject private var mainViewModel: MainViewModel
     @State private var selectedItemToShowQR: EventEntity?
+    @State private var deleteAllAlert = false
 
     @Query(sort: \EventEntity.creationDate, order: .reverse) private var events: [EventEntity]
 
@@ -29,55 +30,71 @@ struct EventsHistoryView: View {
                     }
                 } else {
                     List {
-                        ForEach(events) { event in
-                            Section {
-                                Menu {
-                                    if let urlString = event.downloadURL, let url = URL(string: urlString) {
+                        Section {
+                            ForEach(events) { event in
+                                    Menu {
+                                        if let urlString = event.downloadURL, let url = URL(string: urlString) {
+                                            Button {
+                                                openURL(url)
+                                            } label: {
+                                                HStack {
+                                                    Image(systemName: "calendar.badge.plus")
+                                                    Text("Add to my calendar")
+                                                }
+                                            }
+                                            .disabled(event.expirationDate < Date())
+                                            Button {
+                                                selectedItemToShowQR = event
+                                            } label: {
+                                                HStack {
+                                                    Image(systemName: "qrcode")
+                                                    Text("Show QR code")
+                                                }
+                                            }
+                                            .disabled(event.expirationDate < Date())
+                                        }
                                         Button {
-                                            openURL(url)
+                                            withAnimation {
+                                                mainViewModel.importEvent(event)
+                                            }
                                         } label: {
                                             HStack {
-                                                Image(systemName: "calendar.badge.plus")
-                                                Text("Add to my calendar")
+                                                Image(systemName: "square.and.arrow.down.on.square")
+                                                Text("Import and edit")
                                             }
-                                        }
-                                        .disabled(event.expirationDate < Date())
-                                        Button {
-                                            selectedItemToShowQR = event
-                                        } label: {
-                                            HStack {
-                                                Image(systemName: "qrcode")
-                                                Text("Show QR code")
-                                            }
-                                        }
-                                        .disabled(event.expirationDate < Date())
-                                    }
-                                    Button {
-                                        withAnimation {
-                                            mainViewModel.importEvent(event)
                                         }
                                     } label: {
-                                        HStack {
-                                            Image(systemName: "square.and.arrow.down.on.square")
-                                            Text("Import and edit")
+                                        VStack(alignment: .leading) {
+                                            if !event.fileName.isEmpty {
+                                                Text("\(event.fileName)")
+                                                    .font(.headline)
+                                            }
+                                            Text("\(event.events.count) \(event.events.count == 1 ? "event" : "events")")
+                                            Text("Created on: \(event.creationDate, formatter: dateFormatter)")
+                                                .font(.subheadline)
+                                            Text("Expires on: \(event.expirationDate, formatter: dateFormatter)")
+                                                .font(.subheadline)
                                         }
-                                    }
-                                } label: {
-                                    VStack(alignment: .leading) {
-                                        if !event.fileName.isEmpty {
-                                            Text("\(event.fileName)")
-                                                .font(.headline)
-                                        }
-                                        Text("\(event.events.count) \(event.events.count == 1 ? "event" : "events")")
-                                        Text("Created on: \(event.creationDate, formatter: dateFormatter)")
-                                            .font(.subheadline)
-                                        Text("Expires on: \(event.expirationDate, formatter: dateFormatter)")
-                                            .font(.subheadline)
-                                    }
                                 }
                             }
+                            .onDelete(perform: deleteItems)
+                        } header: {
+                            HStack {
+                                Spacer()
+                                Button("Delete all", role: .destructive) {
+                                    deleteAllAlert = true
+                                }
+                                .alert("Delete all files", isPresented: $deleteAllAlert) {
+                                    Button("Cancel", role: .cancel, action: {})
+                                    Button("Delete", role: .destructive) {
+                                        deleteItems(at: IndexSet(integersIn: events.indices))
+                                    }
+                                } message: {
+                                    Text("Are you sure you want to delete all generated files? This action cannot be undone.")
+                                }
+                            }
+                            .textCase(nil)
                         }
-                        .onDelete(perform: deleteItems)
                     }
                 }
             }
